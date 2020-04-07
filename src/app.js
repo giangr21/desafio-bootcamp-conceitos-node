293f4507059590d3809d900f4572c8078544962a
Gian <giangr21@gmail.com>
2020-04-07 20:41:25 -0300

    finalizando desafio

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +10,65 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+  const repository = {
+    id: uuid(),
+    title: title,
+    url: url,
+    techs: techs,
+    likes: 0
+  }
+  repositories.push(repository)
+  return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { title, url, techs } = request.body
+  if (isUuid(id)) {
+    const i = repositories.findIndex(r => r.id === id);
+    if (i >= 0) {
+      repositories[i].title = title
+      repositories[i].url = url
+      repositories[i].techs = techs
+      return response.json(repositories[i]);
+    }
+    return response.status(400).json({ Error: 'Repositories not found' })
+  } else {
+    return response.status(400).json({ error: 'Invalid ID' })
+  }
 });
 
 app.delete("/repositories/:id", (req, res) => {
-  // TODO
+  const { id } = req.params
+  if (isUuid(id)) {
+    const i = repositories.findIndex(r => r.id === id);
+    if (i < 0) {
+      return res.status(400).json({ Error: 'Repositories not found' })
+    }
+    repositories.splice(i, 1)
+    return res.status(204).send();
+  } else {
+    return res.status(400).json({ error: 'Invalid ID' })
+  }
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+  if (isUuid(id)) {
+    const i = repositories.findIndex(r => r.id === id);
+    if (i >= 0) {
+      repositories[i].likes += 1
+      return response.json(repositories[i])
+    }
+    return response.status(400).json({ Error: 'Repositories not found' })
+  } else {
+    return response.status(400).json({ error: 'Invalid ID' })
+  }
 });
 
 module.exports = app;
